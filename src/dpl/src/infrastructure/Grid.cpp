29f17616367083dfdee6bbe3a0195a5ad16eeb57
hyx <2153478753@qq.com>
2026-03-19 10:59:44 -0700

    fix improve_placement crash

--- src/dpl/src/infrastructure/Grid.cpp
+++ src/dpl/src/infrastructure/Grid.cpp
@@ -353,7 +353,7 @@ GridY Grid::gridHeight(odb::dbMaster* master) const
     return GridY{max(1, divCeil(bbox.dy(), row_height.v))};
   }
   auto site = master->getSite();
-  if (!site->hasRowPattern()) {
+  if (site == nullptr || !site->hasRowPattern()) {
     return GridY{1};
   }
 
@@ -370,7 +370,7 @@ GridY Grid::gridHeight(const Node* cell) const
     return GridY{1};
   }
   auto site = cell->getDbInst()->getMaster()->getSite();
-  if (!site->hasRowPattern()) {
+  if (site == nullptr || !site->hasRowPattern()) {
     return GridY{1};
   }
 
